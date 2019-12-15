07584d846f460fa5622bf67eb4d418c6f24ce51c
zhangjingwen <zhangjingwen2008@163.com>
2019-12-15 23:44:36 +0800

    新增接口：删除客户

--- src/main/java/com/zhangjingwen/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/zhangjingwen/service/impl/CustomerServiceImpl.java
@@ -15,6 +15,7 @@ public class CustomerServiceImpl implements CustomerService {
     @Autowired
     CustomerMapper customerMapper;
 
+    //增加客户
     @Override
     public ResponseResult add(Customer customer) {
         //新增用户
@@ -28,21 +29,30 @@ public class CustomerServiceImpl implements CustomerService {
         return new ResponseResult(CommonCode.SUCCESS);
     }
 
+    //删除客户
     @Override
     public ResponseResult delete(int customerId) {
-        return null;
+        int result = customerMapper.delete(customerId);
+        if (result == 0) {
+            return new ResponseResult(CustomerCode.CUSTOMER_DELETE_FAIL);
+        }
+
+        return new ResponseResult(CommonCode.SUCCESS);
     }
 
+    //更新客户
     @Override
     public ResponseResult update(Customer customer) {
         return null;
     }
 
+    //根据id查找客户
     @Override
     public ResponseResult findById(int customerId) {
         return null;
     }
 
+    //客户列表
     @Override
     public ResponseResult list(int start, int size) {
         return null;
