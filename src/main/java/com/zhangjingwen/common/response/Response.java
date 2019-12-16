57035bc619ac9e09c19f2237dd7ab7b41f3a7c40
zhangjingwen <zhangjingwen2008@163.com>
2019-12-16 09:39:11 +0800

    重构

--- src/main/java/com/zhangjingwen/common/response/Response.java
+++
@@ -1,6 +0,0 @@
-package com.zhangjingwen.common.response;
-
-public interface Response {
-    public static final boolean SUCCESS = true;
-    public static final int SUCCESS_CODE = 1000;
-}
--- src/main/java/com/zhangjingwen/common/response/ResultCode.java
+++
@@ -1,9 +0,0 @@
-package com.zhangjingwen.common.response;
-
-public interface ResultCode {
-    boolean success();
-
-    int code();
-
-    String message();
-}
